2a18f9f5bef61cc4d217af9bed7415b0cfaa88d1
Facecreep <sabitovalen@mail.ru>
2025-04-11 20:00:33 +0300

    Modified project for CLion, removed Werror

---
+++ cmake-build-debug/CMakeFiles/test_version.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Faceecreep/Otus_Homework/test_version.cpp" "C:/Users/Faceecreep/Otus_Homework/cmake-build-debug/CMakeFiles/test_version.dir/test_version.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_TEST_DYN_LINK"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/Apps/MinGW/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/Users/Faceecreep/Otus_Homework/cmake-build-debug/CMakeFiles/helloworld.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
